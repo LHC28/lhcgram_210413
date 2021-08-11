6a1a4fe1c1f2204087022607d7ddde872138cdd5
DESKTOP-HLUUASN\zzang <zzangth95@naver.com>
2021-08-11 17:21:24 +0900

    초기 세팅 설정

---
+++ src/test/java/com/lhcgram/LhcgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhcgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhcgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
